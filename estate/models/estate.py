c3f82c5fb92fd7fbfa7b6391670294c113214767
Louis Horé <loho@odoo.com>
2024-07-02 11:21:13 +0200

    retirer kanban state

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -1,11 +1,6 @@
 from odoo import models, fields
 from dateutil.relativedelta import relativedelta
 
-ESTATE_STATUS = [
-    ('draft',"Brouillon"),
-    ('sent',"Envoyé"),
-    ('confirmed',"Confirmé"),
-]
 
 class Estate(models.Model):
     _name= "estate"
@@ -18,13 +13,4 @@ class Estate(models.Model):
 
     date_availability = fields.Date(default=lambda self: fields.Datetime.today() + relativedelta(months=3), copy=False)
     active = fields.Boolean(default=True)
-    state = fields.Selection(
-        selection=[
-            ('new','New'),
-            ('ongoing','Ongoing'),
-            ('done','Done'),
-            ('cancel','Cancel'),
-        ],
-        string="Status", copy=False, tracking=3, default='new')
-
-    stage = fields.Selection(selection=ESTATE_STATUS, string="Status", readonly=True, copy=False, index=True, tracking=3, default='draft')
+    kanban_state = fields.Selection([('normal', 'In Progress'), ('done', 'Done'), ('blocked', 'Blocked')], default='normal', copy=False)
